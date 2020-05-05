1d25c87c51f6b47ebd9f2548a230823630cbe9ea
Rafael Duarte <rafaeljfduarte@gmail.com>
2020-05-05 10:01:40 +0100

    Template and styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="root">
+      <Title v-bind:key="info.title" v-bind:img="info.title.img" v-bind:username="info.title.username" v-bind:name="info.title.name" v-bind:job="info.title.job"/>
+    </div>
+    <div class="tree">
+      <Leaf
+        v-for="link in info.links"
+        v-bind:key="link"
+        v-bind:link="link.href"
+        v-bind:label_under="link.label_under"
+        v-bind:label_top="link.label_top"
+      ></Leaf>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,7 +23,7 @@ import info from "../public/data.json";
 export default {
   name: "App",
   data() {
-    return info;
+    return {info};
   },
   components: {
     Title,
@@ -20,12 +33,42 @@ export default {
 </script>
 
 <style>
+body {margin: 0; background-color: #FEFFFF; box-sizing: border-box; font-family: 'Roboto', sans-serif;}
+
+a {font-family: 'Roboto', sans-serif; font-weight: 700; font-size: 20px; line-height: 20px;}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #feffff;
+  background-color: #06d6a0;
+  position: relative;
+  width: 100%;
+  max-width: 560px;
+  min-height: 100vh;
+  margin: auto;
+  padding: 60px 30px;
+  box-shadow: 0 0 15px rgba(0,0,0,0.3);
+  box-sizing: border-box;
+}
+
+.root {
+  display: block;
+  position: relative;
+  margin-bottom: 60px;
+}
+
+.tree {
+  width: 100%;
+  max-width: 450px;
+  margin: auto;
+}
+
+@media screen and (max-width: 500px) {
+  .tree {
+    max-width: 320px;
+  }
 }
 </style>
