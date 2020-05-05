7f15f7f2aa847117e4917247929167ca51d28418
Rafael Duarte <rafaeljfduarte@gmail.com>
2020-05-05 11:06:28 +0100

    Data and visuals update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <div class="root">
-      <Title v-bind:key="info.title" v-bind:img="info.title.img" v-bind:username="info.title.username" v-bind:name="info.title.name" v-bind:job="info.title.job"/>
+      <div class="title">
+        <Title v-bind:key="info.title" v-bind:img="info.title.img" v-bind:username="info.title.username" v-bind:name="info.title.name" v-bind:job="info.title.job"/>
+      </div>
     </div>
     <div class="tree">
       <Leaf
@@ -11,6 +13,9 @@
         v-bind:label_under="link.label_under"
         v-bind:label_top="link.label_top"
       ></Leaf>
+    </div>
+    <div class="logo">
+      <img v-bind:key="info.app" :src="info.app.logo" :alt="info.app.name">
     </div>
   </div>
 </template>
@@ -33,7 +38,9 @@ export default {
 </script>
 
 <style>
-body {margin: 0; background-color: #17242A; box-sizing: border-box; font-family: 'Roboto', sans-serif;}
+* {box-sizing: border-box;}
+
+body {margin: 0; background-color: #17242A; font-family: 'Roboto', sans-serif;}
 
 a {font-family: 'Roboto', sans-serif; font-weight: 700; font-size: 20px; line-height: 20px;}
 
@@ -43,27 +50,45 @@ a {font-family: 'Roboto', sans-serif; font-weight: 700; font-size: 20px; line-he
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #17242A;
-  background-color: #06D6A0;
+  background-color: #feffff;
   position: relative;
+  display: block;
   width: 100%;
   max-width: 560px;
   min-height: 100vh;
   margin: auto;
-  padding: 60px 30px;
   box-shadow: 0 0 15px rgba(0,0,0,0.3);
-  box-sizing: border-box;
 }
 
 .root {
   display: block;
   position: relative;
-  margin-bottom: 60px;
+}
+
+.title {
+  width: 100%;
+  padding: 45px 30px;
 }
 
 .tree {
   width: 100%;
   max-width: 450px;
   margin: auto;
+  padding: 30px 15px 120px;
+}
+
+.logo {
+  padding: 30px 15px;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+
+.logo img {
+  max-width: 75px;
 }
 
 @media screen and (max-width: 500px) {
